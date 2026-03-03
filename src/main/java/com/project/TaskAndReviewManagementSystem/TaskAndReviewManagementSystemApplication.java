058a56f12d060c904641af2736ed38fa08c64171
aniketraut16 <rautnaniket@gmail.com>
2026-03-03 18:28:05 +0530

    Initiated Project

---
+++ src/main/java/com/project/TaskAndReviewManagementSystem/TaskAndReviewManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.TaskAndReviewManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAndReviewManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAndReviewManagementSystemApplication.class, args);
+	}
+
+}
